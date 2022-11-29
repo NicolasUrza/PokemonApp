3675be73be0162e69233e1ef0e18d9396abcb155
NicolasUrza <88108043+NicolasUrza@users.noreply.github.com>
2022-11-29 20:58:34 -0300

    fixed bootstrap issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     FontAwesomeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HeaderComponent]
 })
 export class AppModule { }
--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -9,7 +9,7 @@ import { VersionGroupFlavorText } from 'src/app/interfaces/version-group-flavor-
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
-  styleUrls: ['./items.component.sass']
+  styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
   faCircleArrowLeft = faCircleArrowLeft;
